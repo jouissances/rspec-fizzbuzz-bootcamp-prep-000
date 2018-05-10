0885a998144dd54bdf8b0a33873699b12a6450eb
Janice Darikho <janicedarikho@gmail.com>
2018-05-10 14:28:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(integer)
-  if integer % 3 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
     puts "Fizz"
-  elsif integer % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  elsif integer % 15 == 0
+  elsif int % 15 == 0
     puts "Fizzbuzz"
   else
     puts "nil"
